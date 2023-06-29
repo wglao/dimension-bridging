958df78ee556b1053e31c1ac216389e936d0916b
= <wesleygarlao@utexas.com>
2023-06-28 19:28:35 -0500

    checkpointing

--- code/dba/main.py
+++ code/dba/main.py
@@ -5,6 +5,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument("--case-name", default="dba", type=str)
 parser.add_argument("--channels", default=10, type=int)
 parser.add_argument("--latent-sz", default=10, type=int)
 parser.add_argument("--pooling-layers", default=1, type=int)
@@ -14,6 +15,8 @@ parser.add_argument("--wandb", default=0, type=int)
 
 args = parser.parse_args()
 wandb_upload = bool(args.wandb)
+case_name = "_".join(
+    [str(key) + "-" + str(value) for key, value in vars(args).items()])[10:]
 
 import flax.core.frozen_dict as fd
 import flax.linen as nn
@@ -27,16 +30,19 @@ from jax.lax import scan
 import jax.experimental.sparse as jxs
 import pyvista as pv
 from torch.utils.data import DataLoader
+import orbax.checkpoint as orb
 
 from models import GraphEncoder, GraphDecoder, GraphEncoderNoPooling, GraphDecoderNoPooling
 from graphdata import GraphDataset, SpLoader
 from vtk2adj import v2a, combineAdjacency
 
-
 # loop through folders and load data
 ma_list = [0.2, 0.35, 0.5, 0.65, 0.8]
+# ma_list = [0.5]
 re_list = [1e5, 1e6, 1e7, 1e8]
+# re_list = [1e5]
 aoa_list = [0, 2, 4, 6, 8, 10, 12]
+# aoa_list = [0]
 n_slices = 5
 data_path = os.path.join(os.environ["SCRATCH"], "ORNL/dimension-bridging/data")
 
@@ -62,7 +68,8 @@ init_data_3, init_data_2, init_adj_3, init_adj_2 = [
 # ge_3 = GraphEncoder(n_pools, args.latent_sz, args.channels, dim=3)
 # ge_2 = GraphEncoder(n_pools, args.latent_sz, args.channels, dim=2)
 ge_3 = GraphEncoderNoPooling(n_pools, args.latent_sz, args.channels, dim=3)
-ge_2 = GraphEncoderNoPooling(n_pools, args.latent_sz, args.channels, dim=3)    # slices have 3d coords
+ge_2 = GraphEncoderNoPooling(
+    n_pools, args.latent_sz, args.channels, dim=3)  # slices have 3d coords
 final_sz = init_data_3.shape[-1] - 3
 # gd = GraphDecoder(n_pools, final_sz, args.channels, dim=3)
 gd = GraphDecoderNoPooling(n_pools, final_sz, args.channels, dim=3)
@@ -72,6 +79,10 @@ pe_2 = ge_2.init(rng, init_data_2, init_adj_2)['params']
 f_latent, a, c, s = ge_3.apply({'params': pe_3}, init_data_3, init_adj_3)
 pd = gd.init(rng, f_latent, a, c, s)['params']
 params = [pe_3, pe_2, pd]
+
+check = orb.PyTreeCheckpointer()
+check.save(os.path.join(data_path, "models", case_name + "_init"), params)
+
 tx = optax.adam(1e-3)
 
 n_epochs = 100000
@@ -162,16 +173,25 @@ def train_step(params, opt: optax.OptState, lam_2, lam_dp, data_3, data_2,
   for d3, a3 in zip(data_3, adj_3):
     _, a, c, s = ge_3.apply({'params': params[0]}, d3, a3)
     if a_list == []:
-      a_list = jtr.tree_map(lambda a: a / batches / batch_sz, a)
-      c_list = jtr.tree_map(lambda c: c / batches / batch_sz, c)
-      s_list = jtr.tree_map(lambda s: s / batches / batch_sz, s)
+      a_list = [
+          jxs.BCSR((a_i.data / batch_sz, a_i.indices, a_i.indptr),
+                   shape=a_i.shape) for a_i in a
+      ]
+      c_list = jtr.tree_map(lambda c_i: c_i / batch_sz, c)
+      s_list = [
+          jxs.BCSR((s_i.data / batch_sz, s_i.indices, s_i.indptr),
+                   shape=s_i.shape) for s_i in s
+      ]
     else:
-      a_list = jtr.tree_map(lambda a, a_new: a + a_new/batches/batch_sz, a_list,
-                            a)
-      c_list = jtr.tree_map(lambda c, c_new: c + c_new/batches/batch_sz, c_list,
-                            c)
-      s_list = jtr.tree_map(lambda s, s_new: s + s_new/batches/batch_sz, s_list,
-                            s)
+      a_list = [
+          jxs.BCSR((a_i.data + a_new.data / batch_sz, a_i.indices, a_i.indptr),
+                   shape=a_i.shape) for a_i, a_new in zip(a_list, a)
+      ]
+      c_list = jtr.tree_map(lambda c_i, c_new: c_i + c_new/batch_sz, c_list, c)
+      s_list = [
+          jxs.BCSR((s_i.data + s_new.data / batch_sz, s_i.indices, s_i.indptr),
+                   shape=s_i.shape) for s_i, s_new in zip(s_list, s)
+      ]
 
   return loss, params, opt, a_list, c_list, s_list
 
@@ -181,17 +201,15 @@ def test_step(params, adj_list, coordinates, selection, data_3, data_2, adj_2):
   # @jit
   def loss_fn(params, data_3, data_2, adj_2, adj_list, coordinates, selection):
     loss = 0
-    for fb3, fb2 in zip(data_3, data_2):
-      fl2, _, _, _ = ge_2.apply({'params': params[1]}, fb2, adj_2)
+    for fb3, fb2, adj2 in zip(data_3, data_2, adj_2):
+      fl2, _, _, _ = ge_2.apply({'params': params[1]}, fb2, adj2)
       f = gd.apply({'params': params[2]}, fl2, adj_list, coordinates, selection)
       loss_ae = jnp.mean(jnp.square(f[:, 3:] - fb3[:, 3:]))
       loss = loss + loss_ae/test_sz
     return loss
 
-  test_err = 0
-
-  sample_err = loss_fn(params, data_3, data_2, adj_2, adj_list, coordinates, selection)
-  test_err += sample_err / test_dataset.items
+  test_err = loss_fn(params, data_3, data_2, adj_2, adj_list, coordinates,
+                     selection)
   return test_err
 
 
@@ -218,13 +236,25 @@ def main(params, n_epochs):
                                               args.lambda_dp, data_3, data_2,
                                               adj_3, adj_2)
       if a_list == []:
-        a_list = a
-        c_list = c
-        s_list = s
+        a_list = [
+            jxs.BCSR((a_i.data / batches, a_i.indices, a_i.indptr),
+                     shape=a_i.shape) for a_i in a
+        ]
+        c_list = jtr.tree_map(lambda c_i: c_i / batches, c)
+        s_list = [
+            jxs.BCSR((s_i.data / batches, s_i.indices, s_i.indptr),
+                     shape=s_i.shape) for s_i in s
+        ]
       else:
-        a_list = jtr.tree_map(lambda a, a_new: a + a_new, a_list, a)
-        c_list = jtr.tree_map(lambda c, c_new: c + c_new, c_list, c)
-        s_list = jtr.tree_map(lambda s, s_new: s + s_new, s_list, s)
+        a_list = [
+            jxs.BCSR((a_i.data + a_new.data / batches, a_i.indices, a_i.indptr),
+                     shape=a_i.shape) for a_i, a_new in zip(a_list, a)
+        ]
+        c_list = jtr.tree_map(lambda c_i, c_new: c_i + c_new/batches, c_list, c)
+        s_list = [
+            jxs.BCSR((s_i.data + s_new.data / batches, s_i.indices, s_i.indptr),
+                     shape=s_i.shape) for s_i, s_new in zip(s_list, s)
+        ]
     data_3, data_2, adj_3, adj_2 = next(iter(test_dataloader))
     test_err = test_step(params, a, c, s, data_3, data_2, adj_2)
     if epoch % 100 == 0 or epoch == n_epochs - 1:
@@ -237,6 +267,9 @@ def main(params, n_epochs):
       else:
         print("Loss: {:g}, Error {:g}, Epoch {:g}".format(
             loss, test_err, epoch))
+      check.save(
+          os.path.join(data_path, "models",
+                       case_name + "_ep-{:g}".format(epoch)), params)
 
 
 if __name__ == "__main__":
