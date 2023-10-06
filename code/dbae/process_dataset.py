16f4d8224b98c040986be11673a30a235de66d71
= <wesleygarlao@utexas.edu>
2023-10-06 10:39:04 -0500

    fix format and split

--- code/dbae/process_dataset.py
+++ code/dbae/process_dataset.py
@@ -21,8 +21,8 @@ args = parser.parse_args()
 
 # loop through folders and load data
 # ma_list = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8]
-train_ma_list = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45 0.5, 0.55, 0.6]
-test_ma_list = [0.65, 0.7, 0.75, 0.8]
+train_ma_list = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65]
+test_ma_list = [0.7, 0.75, 0.8]
 # re_list = [1e6, 2e6, 3e6, 4e6, 5e6, 6e6, 7e6, 8e6, 9e6, 1e7]
 train_re_list = [2e6, 3e6, 4e6, 5e6, 6e6]
 test_re_list = [7e6, 8e6, 9e6]
@@ -35,38 +35,78 @@ data_path = os.path.join(os.environ["SCRATCH"], "ORNL/dimension-bridging/data")
 
 if args.dataset == 0:
     process_dataset = PairDataset(
-        data_path, train_ma_list, train_re_list, train_aoa_list, "train_coarse_{:d}s".format(n_slices), n_slices
+        data_path,
+        train_ma_list,
+        train_re_list,
+        train_aoa_list,
+        "train_coarse_{:d}s".format(n_slices),
+        n_slices,
     )
 elif args.dataset == 1:
     process_dataset = PairDataset(
-        data_path, test_ma_list, test_re_list, test_aoa_list, "test_coarse_{:d}s".format(n_slices), n_slices
+        data_path,
+        test_ma_list,
+        test_re_list,
+        test_aoa_list,
+        "test_coarse_{:d}s".format(n_slices),
+        n_slices,
     )
 
 if args.dataset == 2:
     process_dataset = PairDataset(
-        data_path, train_ma_list, train_re_list, train_aoa_list, "train_fine_{:d}s".format(n_slices), n_slices
+        data_path,
+        train_ma_list,
+        train_re_list,
+        train_aoa_list,
+        "train_fine_{:d}s".format(n_slices),
+        n_slices,
     )
 elif args.dataset == 3:
     process_dataset = PairDataset(
-        data_path, test_ma_list, test_re_list, test_aoa_list, "test_fine_{:d}s".format(n_slices), n_slices
+        data_path,
+        test_ma_list,
+        test_re_list,
+        test_aoa_list,
+        "test_fine_{:d}s".format(n_slices),
+        n_slices,
     )
 
 elif args.dataset == 4:
     process_dataset = PairDataset(
-        data_path, [0.3, 0.4], [3e6, 4e6], [3, 4], "idev-train_coarse_{:d}s".format(n_slices), n_slices
+        data_path,
+        [0.3, 0.4],
+        [3e6, 4e6],
+        [3, 4],
+        "idev-train_coarse_{:d}s".format(n_slices),
+        n_slices,
     )
 elif args.dataset == 5:
     process_dataset = PairDataset(
-        data_path, [0.5, 0.6], [5e6, 6e6], [5, 6], "idev-test_coarse_{:d}s".format(n_slices), n_slices
+        data_path,
+        [0.5, 0.6],
+        [5e6, 6e6],
+        [5, 6],
+        "idev-test_coarse_{:d}s".format(n_slices),
+        n_slices,
     )
 
 elif args.dataset == 6:
     process_dataset = PairDataset(
-        data_path, [0.3, 0.4], [3e6, 4e6], [3, 4], "idev-train_fine_{:d}s".format(n_slices), n_slices
+        data_path,
+        [0.3, 0.4],
+        [3e6, 4e6],
+        [3, 4],
+        "idev-train_fine_{:d}s".format(n_slices),
+        n_slices,
     )
 elif args.dataset == 7:
     process_dataset = PairDataset(
-        data_path, [0.5, 0.6], [5e6, 6e6], [5, 6], "idev-test_fine_{:d}s".format(n_slices), n_slices
+        data_path,
+        [0.5, 0.6],
+        [5e6, 6e6],
+        [5, 6],
+        "idev-test_fine_{:d}s".format(n_slices),
+        n_slices,
     )
 
 elif args.dataset == 8:
